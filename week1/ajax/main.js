c39bf8a2497f03d747699995af6feb02e328b920
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-10-18 13:37:26 -0500

    ajax demo.

---
+++ week1/ajax/main.js
@@ -0,0 +1,77 @@
+// AJAX
+// asynchronous JavaScript and XMLHttpRequest
+
+// we're going to send HTTP messages from JavaScript
+// and process the results without ever reloading the page
+
+// a synchronous operation blocks all other activity
+// until it's done.
+
+// an asynchronous operation allows other code to run
+// and will proceed when the operation is done.
+
+function ajax(url, success, failure) {
+    let xhr = new XMLHttpRequest();
+
+    // the "readyState" tells you the progress of
+    // receiving the response.
+    xhr.onreadystatechange = () => {
+        console.log(xhr.readyState);
+        // TODO: handle the response
+        if (xhr.readyState === 4) {
+            // we've received the full response.
+            // it's a JSON string
+            let resJSON = xhr.response;
+            if (xhr.status === 200) { // (success)
+                let resObj = JSON.parse(resJSON);
+                // use our callbacks
+                success(resObj);
+            } else {
+                failure(resJSON, xhr.status);
+            }
+        }
+    };
+
+    // describing the request to be made
+    xhr.open('GET', url);
+
+    // construct and send the request
+    xhr.send();
+    // the next thing that'll happen is
+    // readystatechange will fire a bunch of times
+    console.log("end of ajax function.");
+}
+
+// JSON: JavaScript object notation
+
+// a little bit different from
+// writing JS objects
+{
+    name: "Nick"
+    fn: () => 1;
+}
+
+// JSON:
+// {
+//      "name": "Nick",
+//      "fn": "No functions allowed in JSON"
+// }
+
+document.addEventListener("DOMContentLoaded", () => {
+    let joke = document.getElementById("joke");
+    let jokeBtn = document.getElementById("jokeBtn");
+
+    jokeBtn.addEventListener("click", event => {
+        // params: url, success, failure
+        ajax(
+            "http://api.icndb.com/jokes/random/",
+            obj => {
+                console.log(obj);
+                joke.innerHTML = obj.value.joke;
+            },
+            (res, status) => {
+                console.log(`Failure, status ${status}`);
+            }
+        );
+    });
+});
